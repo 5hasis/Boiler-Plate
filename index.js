099f63e06a961d22c136212a721eefedc68fcbbe
5hasis <dhgkdud4002@naver.com>
2021-08-24 19:02:07 +0900

    postman, nodemon 설치

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!앙뇽')
-})
+//application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}));
+
+//application/json
+//app.use(bodyParser.json());
+app.use(express.json());
+
+//const bodyParser = require('body-parser');
+const{ User } = require("./models/User");
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ohasis:1234@boilerplate.l0hi1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -15,4 +22,25 @@ mongoose.connect('mongodb+srv://ohasis:1234@boilerplate.l0hi1.mongodb.net/myFirs
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!앙뇽~')
+})
+
+//회원가입을 위한 route
+app.post('/register', (req, res) => {
+  //회원가입시 필요한 정보들을 client에서 가져오면
+  //그걸들을 데이터 베이스에 넣어줌
+  
+  const user = new User(req.body)
+
+  //save() : mongoDB에서 오는 메소드
+  user.save((err, userInfo) => {
+    if(err) return res.json({success:false, err}) //에러메세지 함께 전달
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
