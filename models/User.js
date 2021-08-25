afe509e69533bb0bd45b46bc8cfadc12be7a5fe6
5hasis <dhgkdud4002@naver.com>
2021-08-26 00:22:38 +0900

    Bcrypt로 비밀번호 해시 암호화

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 const userSchema = mongoose.Schema({
     name: {
         type:String,
@@ -28,6 +31,24 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type:Number
+    }
+})
+
+userSchema.pre('save', function(next){
+
+    var user = this;
+
+    if(user.isModified('password')){
+        //비밀번호 암호화 시키기
+        bcrypt.genSalt(saltRounds, function(err,salt){
+            if(err) return(err)
+
+            bcrypt.hash(user.password, salt, function(err,hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
     }
 })
 
